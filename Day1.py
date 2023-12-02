5b2e789c779dd8385e97b75bcb9b7a73f91805de
LaseneX <enes@cr34.com>
2023-12-01 21:27:03 -0500

    Updated Both FIles

--- Day1.py
+++ Day1.py
@@ -2,14 +2,20 @@ import sys
 
 # Read the arguement
 A = open(sys.argv[1]).read().strip()
-ans = 0
 
+# Sum of all numbers
+total = 0
+
+# For every new line
 for line in A.split('\n'):
-    digits = []
+    # Create a list and empty it at the end of every for loop
+    numbers = []
+    # find the numbers
     for c in line:
         if c.isdigit():
-            digits.append(c)
-    score = int(digits[0] + digits[-1])
-    ans += score
+            # We add every digit we find
+            numbers.append(c)
+    # merge the first and final digit and add it to sum
+    total += int(numbers[0] + numbers[-1])
 
-print(ans)
+print(total)
--- Day1_2.py
+++ Day1_2.py
@@ -1,17 +1,16 @@
 import sys
 
-D = open(sys.argv[1]).read().strip()
-ans = 0
+A = open(sys.argv[1]).read().strip()
+total = 0
 
-for line in D.split('\n'):
-    digits = []
+for line in A.split('\n'):
+    numbers = []
     for i, c in enumerate(line):
         if c.isdigit():
-            digits.append(c)
+            numbers.append(c)
         for d, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
             if line[i:].startswith(val):
-                digits.append(str(d+1))
-    score = int(digits[0] + digits[-1])
-    ans += score
+                numbers.append(str(d+1))
+    total += int(numbers[0] + numbers[-1])
 
-print(ans)
+print(total)
